741851c3f264874d178ae12640c78e66b01a92e7
ASdOliveira <arysson.silva@hotmail.com>
2019-09-14 11:38:01 -0300

    Function that will create the inicial pop..

---
+++ Genetic Algorithm/population.py
@@ -0,0 +1,24 @@
+"""
+File related to creation, crossover and mutation of the population
+"""
+import random
+
+#creates an aleatory popuplation, which the 1st and the last cities are equals.
+def createPop(vector):
+    createdPop = [None] * (len(vector)+1) 
+
+    for i in range(len(vector)):
+        x = random.choice(vector)
+        if i == 0:
+            createdPop[0] = x  # the 1st and the last one city will be equals.
+            createdPop[len(vector)] = x
+        else:
+            createdPop[i] = x        
+        vector.remove(x) #remove the city, thus, won't exist the repeated cities.
+    return createdPop
+
+def crossover():
+    return
+
+def mutation():
+    return
