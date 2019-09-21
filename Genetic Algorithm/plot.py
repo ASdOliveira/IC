2f5140ad073e8394384a25253400b4469dcd21e2
ASdOliveira <arysson.silva@hotmail.com>
2019-09-21 11:34:08 -0300

    Improving the graph ploting

--- Genetic Algorithm/plot.py
+++ Genetic Algorithm/plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
 def plot(vector):
-    plt.axis([0, 2000, 0, 2000])
-    plt.plot([vector[i][1] for i in range(len(vector))], [vector[i][2] for i in range(len(vector))], 'xb-')
+    plt.axis([0, 1800, 0, 1200])
+    plt.plot([vector[i][1] for i in range(len(vector))], [vector[i][2] for i in range(len(vector))], '.b-')
     plt.show()
