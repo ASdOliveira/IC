cb2952ef6ac0e7a077332ac619850f1a18c019d2
ASdOliveira <arysson.silva@hotmail.com>
2019-10-19 10:28:03 -0300

    Functions that will be optimized

--- PSO/function_opt.py
+++ PSO/function_opt.py
@@ -1,14 +1,15 @@
 #File destinated to fuctions that will be optimized
 
-def func1(x):
+import math
+
+def sphere(x):
     total=0
     for i in range(len(x)):
         total+=x[i]**2
     return total
 
-def sphere():
-    return
-
-
-def rastrigin():
-    return
+def rastrigin(x):
+    total = 0
+    for i in range(len(x)):
+        total +=  (x[i]**2) - 10*(math.cos(2*math.pi*x[i])) + 10
+    return total
